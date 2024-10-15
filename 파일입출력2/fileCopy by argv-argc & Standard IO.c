a7be13e0ba0e6035e0bbb0c515043a8ab1ce85ee
WJLee <146986774+WJLee22@users.noreply.github.com>
2024-10-15 20:16:02 +0900

    Create fileCopy by argv-argc & Standard IO.c

---
+++ 파일입출력2/fileCopy by argv-argc & Standard IO.c
@@ -0,0 +1,49 @@
+#include <sys/types.h> 
+#include <sys/stat.h> 
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
+
+int main(int argv, char* argc[]) {
+    int n;
+    int fd1, fd2;
+    char buf[80];
+
+
+    if (argv < 3) { //if not 3 parameter
+        perror("main argc error");
+        exit(1);
+    }
+
+
+    fd1 = open(argc[1], O_RDONLY);
+    if (fd1 == -1) {
+        perror("Error");
+        exit(1);
+    }
+
+    fd2 = open(argc[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd2 == -1) {
+        perror("Error");
+        close(fd1);
+        exit(1);
+    }
+
+    printf("fdRead = %d\nfdWrite = %d\n\n", fd1, fd2);
+
+
+    n = read(fd1, buf, 5);
+    write(fd2, buf, n);
+    while (n > 0) {
+        n = read(fd1, buf, 5);
+        write(fd2, buf, n);
+    }
+
+    close(fd1);
+    close(fd2);
+
+    printf(" ./NewHansung.txt created!!\n");
+    return 0;
+
+}

07a6ce26cc3dcdbf7d85ee4ffcb1310390498918
WJLee <146986774+WJLee22@users.noreply.github.com>
2024-10-30 04:43:12 +0900

    Update Add_MidTerm.c

--- MidTerm/Add_MidTerm.c
+++ MidTerm/Add_MidTerm.c
@@ -7,80 +7,65 @@
 #include <string.h>
 #include <strings.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char * argv[]){
 
-    char buf[80], pathName[80];
-    int fdWrite, readNum;
+char buf[200], pathName[100];
 
-    if (argc < 3) {
-        perror("agrc < 3 error ");
-        exit(1);
-    }
+if(argc < 3){
+perror("argc < 3 error : ");
+exit(1);
+}
 
-    bzero(buf, sizeof(buf));  //clear	
-    strcat(buf, "==========================================\n");
-    write(1, buf, strlen(buf));
+bzero(buf, sizeof(buf));
+for(int i=0; i<42; i++){
+  strcat(buf,"=");
+}
+strcat(buf, "\n ");
+strcat(buf, "Hansung University, ");
+strcat(buf, argv[1]);
+strcat(buf, ", ");
+strcat(buf, argv[2]);
+strcat(buf, "\n");
+for(int i=0; i<42; i++){
+  strcat(buf,"=");
+}
 
-    bzero(buf, sizeof(buf));
-    strcat(buf, " Hansung University, ");
-    strcat(buf, argv[1]);
-    strcat(buf, ", ");
-    strcat(buf, argv[2]);
-    strcat(buf, " \n");
-    write(1, buf, strlen(buf));
+write(1,buf,strlen(buf));
 
-    bzero(buf, sizeof(buf));
-    strcat(buf, "==========================================\n");
-    write(1, buf, strlen(buf));
+bzero(buf, sizeof(buf));
+strcpy(buf,"\nSource PathName : ");
+write(1,buf,sizeof(buf));
 
+bzero(pathName, sizeof(pathName));
+read(0,pathName,sizeof(pathName));
+pathName[strlen(pathName)-1]='\0';
+int fdWrite;
 
+fdWrite=open(pathName, O_WRONLY | O_APPEND);
+	if(fdWrite == -1){
+		perror("open error ");
+		exit(1);
+	}
 
+while(1){
+bzero(buf,sizeof(buf));
+read(0,buf,sizeof(buf));
 
-    bzero(buf, sizeof(buf));
-    strcat(buf, "Source PathName : ");
-    write(1, buf, strlen(buf));
+if(strcmp(buf,"quit\n")==0)
+  break;
 
+write(fdWrite,buf,strlen(buf));
+}
+bzero(buf,sizeof(buf));
+strcat(buf,pathName);
+strcat(buf," Appended !! Thanks ~~\n");
+write(1,buf,sizeof(buf));
+write(1,argv[1],strlen(argv[1]));
+write(1,"    ",5);
+write(1,argv[2],strlen(argv[2]));
+write(1,"\n",strlen(argv[2]));
 
+close(fdWrite);
 
-    //keyboard input
-    bzero(pathName, sizeof(pathName));
-    read(0, pathName, sizeof(pathName));
-    pathName[strlen(pathName) - 1] = '\0';  
-
-
-
-
-
-    fdWrite = open(pathName, O_WRONLY | O_APPEND);
-    if (fdWrite == -1) {
-        perror("open error");
-        exit(1);
-    }
-
-    while (1) {
-        bzero(buf, sizeof(buf));
-        readNum = read(0, buf, sizeof(buf)); 
-        if (strcmp(buf, "quit\n") == 0) {
-            break;
-        }
-
-        write(fdWrite, buf, readNum); 
-    }
-    close(fdWrite); 
-
-    bzero(buf, sizeof(buf));
-    strcat(buf, pathName);
-    strcat(buf, "  Created !! Thanks ~~ \n");
-    write(1, buf, strlen(buf));
-
-    
-        bzero(buf, sizeof(buf));
-        strcat(buf, argv[1]);
-        strcat(buf, "    ");
-        strcat(buf, argv[2]);
-        strcat(buf, "\n");
-        write(1, buf, strlen(buf));
-  
-
-    return 0;
+return 0;
 }
